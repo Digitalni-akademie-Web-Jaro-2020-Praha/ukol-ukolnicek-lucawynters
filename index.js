1d0a34a7db87cdee69ea522bae323a639119971a
Lucie Zýmová <zyminka@gmail.com>
2020-05-16 17:26:33 +0200

    vytvoření queryselectorů

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log('funguju!');
+
+const button = document.querySelector('button');
+const inputElm = document.querySelector('input');
+const todoList = document.querySelector('.todo__tasks');
+const newTask = inputElm.value;
+
