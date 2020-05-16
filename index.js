d4d995c28768cc211f4b496f5ea4bac05c83c03d
Lucie Zýmová <zyminka@gmail.com>
2020-05-16 17:37:08 +0200

    fce btnClick

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ console.log('funguju!');
 
 const button = document.querySelector('button');
 const inputElm = document.querySelector('input');
-const todoList = document.querySelector('.todo__tasks');
-const newTask = inputElm.value;
+const newTask = () => {
+  return inputElm.value
+}
+const btnClick = (event) => {
+  const todoList = document.querySelector('.todo__tasks');
+  todoList.innerHTML = `<div class="task">
+  Vyprat ponožky
+</div>
+<div class="task">
+  Naučit se funkce vyššího řádu
+</div>
+<div class="task">
+  Nakoupit na víkend
+</div><div>${newTask}</div>`;
+}
 
