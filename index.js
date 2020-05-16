3e37ae84456816e037e21cc33c1b9bc41b7ed2e6
Lucie Zýmová <zyminka@gmail.com>
2020-05-16 20:01:12 +0200

    rename const + clear input

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 console.log('funguju!');
 
 const button = document.querySelector('button');
-const inputElm = document.querySelector('input');
-const newTask = () => {
-  return inputElm.value
+const tasks = document.querySelector('input');
+const updateTasks = () => {
+  return tasks.value
 }
 const btnClick = (event) => {
   const todoList = document.querySelector('.todo__tasks');
-  todoList.innerHTML += `<div class="task">${newTask()}</div`;
-
+  todoList.innerHTML += `<div class="task">${updateTasks()}</div`;
+  /*const tasks = document.querySelector('input');*/
+  tasks.value = tasks.defaultValue;
 }
 
 button.addEventListener('click', btnClick);
