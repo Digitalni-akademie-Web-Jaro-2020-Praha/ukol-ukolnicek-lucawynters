9d8354e35eb6e12971d65d915fdcbef5b20e2a06
Lucie Zýmová <zyminka@gmail.com>
2020-05-16 19:52:36 +0200

    final

--- index.js
+++ index.js
@@ -9,15 +9,8 @@ const newTask = () => {
 }
 const btnClick = (event) => {
   const todoList = document.querySelector('.todo__tasks');
-  todoList.innerHTML = `<div class="task">
-  Vyprat ponožky
-</div>
-<div class="task">
-  Naučit se funkce vyššího řádu
-</div>
-<div class="task">
-  Nakoupit na víkend
-</div><div class="task">${newTask()}</div>`;
+  todoList.innerHTML += `<div class="task">${newTask()}</div`;
+
 }
 
 button.addEventListener('click', btnClick);
