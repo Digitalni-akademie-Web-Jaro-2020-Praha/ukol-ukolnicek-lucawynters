02d33f7424d5a36f927cd81d963ac86865100bc1
Lucie Zýmová <zyminka@gmail.com>
2020-05-25 21:04:41 +0200

    updateTasks + event

--- index.js
+++ index.js
@@ -8,21 +8,22 @@ const todoList = [
   'Nakoupit na víkend',
 ];
 
-const listElm = document.querySelector('.todo__tasks');
-for (let i = 0; i < todoList.length; i += 1) {
-  listElm.innerHTML += `<li>${todoList[i]}</li>`;
-}
+const updateTasks = () => {
+  const listElm = document.querySelector('.todo__tasks');
+  listElm.innerHTML = '';
+  for (let i = 0; i < todoList.length; i += 1) {
+    listElm.innerHTML += `<li>${todoList[i]}</li>`;
+  }
+};
 
-/*
 const button = document.querySelector('button');
 const tasks = document.querySelector('input');
-const updateTasks = () => {
-  return tasks.value
-}
+
 const btnClick = (event) => {
-  const todoList = document.querySelector('.todo__tasks');
-  todoList.innerHTML += `<div class="task">${updateTasks()}</div`;
+
+  todoList.push(tasks.value)
+  updateTasks()
   tasks.value = tasks.defaultValue;
 }
 
-button.addEventListener('click', btnClick);*/
+button.addEventListener('click', btnClick);
